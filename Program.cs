d4c4e74855dc0510a74024d0beec516b25d60e57
Eugene <80662982619q@gmail.com>
2022-11-22 13:58:10 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,86 @@
+﻿using System.Reflection;
+
+namespace Reflection
+    {
+    internal class Program
+        {
+
+        static void Main(string[] args)
+            {
+
+
+           /* Загружаем сборку ClassLibraryForReflection*/
+            Assembly assem = null;
+            try
+                {
+                assem = Assembly.LoadFrom(".\\ClassLibraryForReflection.dll");
+                }
+            catch (FileNotFoundException ex)
+                {
+
+                Console.WriteLine("no ok");
+                }
+
+
+            Type ?type = assem.GetType("ClassLibraryForReflection.Class2");
+            Type ?atr = assem.GetType("ClassLibraryForReflection.MyAttribute");
+
+
+            Programs.Go(type);
+
+
+            object? v = Activator.CreateInstance(type);
+
+           /* получаем методі Class2*/
+            MethodInfo[] methodInfos = type.GetMethods(BindingFlags.DeclaredOnly |
+            BindingFlags.Public |
+            BindingFlags.Instance);
+
+
+
+
+            Console.WriteLine(Environment.NewLine+ "вызываем методы динамическим связыванием по признаку своего атрибута MyAttribute" + Environment.NewLine);
+            foreach (var item in methodInfos)
+                {
+              
+                var tt = item.GetCustomAttributes(atr,false);
+
+                /*параметрі*/
+                ParameterInfo[] parameterInfos = item.GetParameters();
+                /* ищем методі с кастомнім аттрибутом и 2 инт параметрами*/
+                if (
+tt.Count()!=0
+               &&
+                parameterInfos.Count() == 2 &&
+                parameterInfos.All(q => q.ParameterType.FullName == "System.Int32")
+                ) {
+
+               /* візіваем методі динамическим связіванием*/
+                    object[] temp = new object[] { 100, 50 };
+                    Console.WriteLine(item.Invoke(v, temp));
+
+
+
+                    }
+                }
+
+
+
+
+
+
+
+
+
+
+
+
+   
+
+
+
+            }
+        }
+
+
+    }
---
+++ Worker.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reflection
+    {
+   
+
+        static class Programs
+            {
+            // Получаем разную информацию
+            static void ListVariosStats(Type t)
+                {
+                Console.WriteLine(new string('_', 30) + " Информация о Class1" + "\n");
+            
+
+                Console.WriteLine("Полное Имя:             {0}", t.FullName);
+                Console.WriteLine("Базовый класс:          {0}", t.BaseType);
+                Console.WriteLine("Абстрактный:            {0}", t.IsAbstract);
+                Console.WriteLine("Запрещено наследование: {0}", t.IsSealed);
+                Console.WriteLine("Это class:              {0}", t.IsClass);
+                }
+
+            // Получаем информацию об Именах всех методов
+            static void ListMethods(Type t)
+                {
+                Console.WriteLine(new string('_', 30) + " Методы класса Class1" + "\n");
+              
+                MethodInfo[] mi = t.GetMethods(BindingFlags.Instance
+                        | BindingFlags.Static
+                        | BindingFlags.Public
+                        | BindingFlags.NonPublic | BindingFlags.DeclaredOnly);
+
+                foreach (MethodInfo m in mi)
+                    Console.WriteLine("Method: {0}", m.Name);
+                }
+
+            // Получаем информацию об Именах полей 
+            static void ListFields(Type t)
+                {
+                Console.WriteLine(new string('_', 30) + " Поля класса Class1" + "\n");
+
+               
+                FieldInfo[] fi =
+                    t.GetFields(BindingFlags.Instance
+                        | BindingFlags.Static
+                        | BindingFlags.Public
+                        | BindingFlags.NonPublic);
+
+                foreach (FieldInfo f in fi)
+                    Console.WriteLine("Field: {0}", f.Name);
+                }
+
+            //  Свойства
+            static void ListProps(Type t)
+                {
+                Console.WriteLine(new string('_', 30) + " Свойства класса Class1" + "\n");
+               
+                PropertyInfo[] pi = t.GetProperties();
+
+                foreach (PropertyInfo p in pi)
+                    Console.WriteLine("Свойство: {0}", p.Name);
+                }
+
+            //  Интерфейсі
+            static void ListInterfaces(Type t)
+                {
+                Console.WriteLine(new string('_', 30) + " Интерфейсы класса Class1" + "\n");
+            
+
+                Type[] it = t.GetInterfaces();
+
+                foreach (Type i in it)
+                    Console.WriteLine("Интерфейс: {0}", i.Name);
+                }
+
+            // конструкторі 
+            static void ListConstructors(Type t)
+                {
+                Console.WriteLine(new string('_', 30) + " Конструкторы класса Class1" + "\n");
+
+               
+                ConstructorInfo[] ci = t.GetConstructors();
+
+                foreach (ConstructorInfo m in ci)
+                    Console.WriteLine("Constructor: {0}", m.Name);
+                }
+
+           public static void Go(Type instance)
+                {
+                Console.SetWindowSize(80, 45);
+
+             
+
+           
+
+                ListVariosStats(instance);  // Получаем разную информацию 
+                ListMethods(instance);      // Получаем информацию об Именах всех методов
+                ListFields(instance);       // Получаем информацию об Именах всех полей
+                ListProps(instance);        // Получаем список всех Свойств 
+                ListInterfaces(instance);   // Получаем список всех Интерфейсов
+                ListConstructors(instance); // Получаем информацию конструкторов
+
+
+                }
+            }
+        }
+    
